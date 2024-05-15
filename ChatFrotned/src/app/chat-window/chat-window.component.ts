09252cd2a5068aab859234600a8f92fcdd684024
thom9346 <holmegaard1996@gmail.com>
2024-05-15 17:09:32 +0200

    chat now scrolls down automatically

--- ChatFrotned/src/app/chat-window/chat-window.component.ts
+++ ChatFrotned/src/app/chat-window/chat-window.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
 import { ChatService } from '../services/chat.service';
 import { Message } from '../models/Message';
 import { AuthService } from '../services/auth.service';
@@ -11,11 +11,13 @@ import { SignalRService } from '../services/signal-r.service';
   templateUrl: './chat-window.component.html',
   styleUrls: ['./chat-window.component.scss']
 })
-export class ChatWindowComponent implements OnInit {
+export class ChatWindowComponent implements OnInit, AfterViewChecked {
 
   messages: Message[] = [];
   selectedRecipient: User | null = null;
   currentUser: any;
+
+  @ViewChild('messagesContainer') private messagesContainer!: ElementRef;
 
   constructor(private chatService: ChatService, private authService: AuthService, private router: Router, private signalRService: SignalRService) {
     this.currentUser = this.authService.getCurrentUser();
@@ -25,14 +27,21 @@ export class ChatWindowComponent implements OnInit {
     this.signalRService.messageReceived$.subscribe((message: Message) => {
       if (this.selectedRecipient && (message.senderId === this.selectedRecipient.userId || message.recipientId === this.selectedRecipient.userId)) {
         this.messages.push(message);
+        this.scrollToBottom();
       }
     });
+  }
+  ngAfterViewChecked() {
+    this.scrollToBottom();
   }
 
   loadMessages() {
     if (this.selectedRecipient) {
       this.chatService.getMessagesBetweenUsers(this.currentUser.nameid, this.selectedRecipient.userId).subscribe({
-        next: (data) => this.messages = data,
+        next: (data) => {
+          this.messages = data,
+          this.scrollToBottom();
+        }, 
         error: (error) => console.error('Failed to get messages:', error)
       });
     }
@@ -46,5 +55,12 @@ export class ChatWindowComponent implements OnInit {
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+  private scrollToBottom(): void {
+    try {
+      this.messagesContainer.nativeElement.scrollTop = this.messagesContainer.nativeElement.scrollHeight;
+    } catch (err) {
+      console.error('Could not scroll to bottom:', err);
+    }
   }
 }
